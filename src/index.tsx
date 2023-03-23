4cc55bf8b8599a37fad67d42a09465e441793211
HsinYu <hsinyu.yang@hwacom>
2023-03-24 00:05:50 +0800

    first webpack typescript project

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+// import {Hello} from "./components/Hello";
+
+const APP = () => (
+  <div>Hello World</div>
+);
+
+render(<APP/>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+//   optimization: {
+//     runtimeChunk: 'single',
+//   },
+};
