12845fcab2099e6c78c81bbab9b2758a379ccb7b
HsinYu <hsinyu.yang@hwacom>
2023-05-08 00:00:53 +0800

    header finish

--- src/containers/Layout/components/Header/Header.tsx
+++ src/containers/Layout/components/Header/Header.tsx
@@ -1,9 +1,29 @@
-import * as React from 'react';
-import * as Style from './styles';
-const Header = ()=>{
-    return <Style.HeaderDiv>
-                <Style.HeaderLogo>Brady</Style.HeaderLogo>
-                <Style.HeaderNavGroup>Nav</Style.HeaderNavGroup>
-        </Style.HeaderDiv>
-}
+import * as React from "react";
+import * as Style from "./styles";
+const Header = () => {
+  return (
+    <Style.HeaderDiv>
+      <Style.HeaderLogo>Brady</Style.HeaderLogo>
+      <Style.HeaderNavGroup>
+        <Style.HeaderNav>
+          <Style.HeaderNavList>
+            <Style.HeaderNavLink href="#">Home</Style.HeaderNavLink>
+          </Style.HeaderNavList>
+          <Style.HeaderNavList>
+            <Style.HeaderNavLink href="#">About</Style.HeaderNavLink>
+          </Style.HeaderNavList>
+          <Style.HeaderNavList>
+            <Style.HeaderNavLink href="#">Projects</Style.HeaderNavLink>
+          </Style.HeaderNavList>
+          <Style.HeaderNavList>
+            <Style.HeaderNavLink href="#">Articles</Style.HeaderNavLink>
+          </Style.HeaderNavList>
+          <Style.HeaderNavList>
+            <Style.HeaderNavLink href="#">Contact</Style.HeaderNavLink>
+          </Style.HeaderNavList>
+        </Style.HeaderNav>
+      </Style.HeaderNavGroup>
+    </Style.HeaderDiv>
+  );
+};
 export default Header;
--- src/containers/Layout/components/Header/styles.tsx
+++ src/containers/Layout/components/Header/styles.tsx
@@ -1,26 +1,56 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
 export const HeaderDiv = styled.div`
   width: 100%;
-  height: 200px;
+  // height: 200px;
   background-color: rgb(56, 56, 56);
   display: flex;
   justify-content: space-between;
   flex-wrap: nowrap;
+  padding: 0px 100px;
 `;
 
 export const HeaderLogo = styled.div`
-  
+  @import url("https://fonts.googleapis.com/css2?family=Heebo&display=swap");
+  font-size: 24px;
+  font-family: "Heebo", sans-serif;
+  color: white;
+  padding: 10px;
 `;
 
 export const HeaderNavGroup = styled.div`
   display: flex;
   justify-content: space-between;
+  width: 30%;
 `;
 
 export const HeaderNav = styled.ul`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
+  width: 100%;
+`;
+export const HeaderNavList = styled.li`
+  // remove li style
+  list-style-type: none;
+  color: white;
+  width: 30%;
 `;
 
-export const HeaderNavLink = styled.li`
+export const HeaderNavLink = styled.a`
+  @import url("https://fonts.googleapis.com/css2?family=Heebo&display=swap");
+  // remove a style
+  color: inherit; /* 移除超連結顏色 */
+  text-decoration: none; /* 移除超連結底線 */
+  display: block; /* 讓 <a> 填滿 <li> */
+
+  cursor: pointer;
+  font-size: 24px;
+  font-family: "Heebo", sans-serif;
+  padding: 10px;
+
+  &:hover {
+    font-size: 36px;
+    color: black;
+    background-color: rgb(115, 115, 115);
+  }
 `;
